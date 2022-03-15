9ca9022158d92b8a9c8eea83297fadf6d16c8b10
teo2mirce <teo2mircea@gmail.com>
2022-03-15 22:27:22 +0200

    Add files via upload

---
+++ Led_Temp.ino
@@ -0,0 +1,115 @@
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+
+#include "DHT.h"
+
+#define DHTPIN 16     // Digital pin connected to the DHT sensor
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 3 (on the right) of the sensor to GROUND (if your sensor has 3 pins)
+// Connect pin 4 (on the right) of the sensor to GROUND and leave the pin 3 EMPTY (if your sensor has 4 pins)
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+
+
+
+
+#include <LiquidCrystal_PCF8574.h>
+#include <Wire.h>
+
+LiquidCrystal_PCF8574 lcd(0x27); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+
+
+#include <string>
+#include <sstream>
+using namespace std;
+
+std::string NumberToString ( float Number )
+{
+   std::ostringstream ss;
+   ss << Number;
+   return ss.str();
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+
+
+
+
+    // See http://playground.arduino.cc/Main/I2cScanner how to test for a I2C device.
+  Wire.begin();
+  Wire.beginTransmission(0x27);
+  int error = Wire.endTransmission();
+  Serial.print("Error: ");
+  Serial.print(error);
+
+  if (error == 0) {
+    Serial.println(": LCD found.");
+    lcd.begin(16, 2); // initialize the lcd
+
+    
+    lcd.setBacklight(10);
+
+  } else {
+    Serial.println(": LCD not found.");
+  } // if
+
+  
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) )
+  {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  string STR;
+  STR="Humidity: "+NumberToString (h)+"%";
+  lcd.print(STR.data());
+  lcd.setCursor(0, 1);
+  STR="Temp: "+NumberToString(t)+"C";
+  lcd.print(STR.data());
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.println();
+  
+}
